a13ea743524171cda201141b16b9cafa4b1f896d
Matheus Pina <mathpinab@gmail.com>
2026-04-20 01:30:49 -0300

    Change default theme in ThemeProvider from dark to light in layout.tsx for improved user experience.

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
       <body className={`${inter.variable} ${inter.className} font-sans`}>
         <ThemeProvider
           attribute="class"
-          defaultTheme="dark"
+          defaultTheme="light"
           enableSystem
           disableTransitionOnChange
         >
